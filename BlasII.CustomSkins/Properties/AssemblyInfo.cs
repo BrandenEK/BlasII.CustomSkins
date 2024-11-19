5f2cacfa30d700d5c1be11f1a6b3936398011326
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-19 07:53:00 -0500

    core: setup csproj

--- BlasII.CustomSkins/Properties/AssemblyInfo.cs
+++ BlasII.CustomSkins/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.CustomSkins;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.CustomSkins.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
+[assembly: MelonOptionalDependencies("Modding API", "1.1.0")]
