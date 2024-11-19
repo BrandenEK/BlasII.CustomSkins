11892a457cedbed09a3ba0bdd37d08f902e8e48e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-19 07:48:40 -0500

    init: add base project files

---
+++ BlasII.CustomSkins/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using BlasII.CustomSkins;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(BlasII.CustomSkins.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
