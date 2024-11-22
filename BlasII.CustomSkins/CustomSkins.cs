e1d988b40ad6b3c5026ba6e153768dd9d286e88e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-22 16:09:07 -0500

    import: move loading code to class

--- BlasII.CustomSkins/CustomSkins.cs
+++ BlasII.CustomSkins/CustomSkins.cs
@@ -1,7 +1,6 @@
 ﻿using BlasII.ModdingAPI;
 using BlasII.ModdingAPI.Helpers;
 using Il2CppTGK.Game;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -15,14 +14,15 @@ public class CustomSkins : BlasIIMod
 {
     internal CustomSkins() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-    private readonly Dictionary<string, Sprite> _loadedSprites = [];
+    private Dictionary<string, Sprite> _loadedSprites = [];
 
     /// <summary>
     /// Load all spritesheets
     /// </summary>
     protected override void OnInitialize()
     {
-        LoadAllSpritesheets();
+        var importer = new Importer(Path.Combine(FileHandler.ModdingFolder, "skins"));
+        _loadedSprites = importer.LoadAllSpritesheets();
     }
 
     /// <summary>
@@ -43,46 +43,6 @@ public class CustomSkins : BlasIIMod
                 continue;
 
             renderer.sprite = customSprite;
-        }
-    }
-
-    private void LoadAllSpritesheets()
-    {
-        ModLog.Warn("Starting Import...");
-
-        // Create skins folder
-        string dir = Path.Combine(FileHandler.ModdingFolder, "skins");
-        Directory.CreateDirectory(dir);
-
-        // Get all json files in the skins folder
-        foreach (var file in Directory.GetFiles(dir, "*.json", SearchOption.TopDirectoryOnly))
-        {
-            LoadSpritesheet(Path.GetFileNameWithoutExtension(file));
-        }
-    }
-
-    private void LoadSpritesheet(string animation)
-    {
-        // Import info list from skins folder
-        var json = File.ReadAllText(Path.Combine(FileHandler.ModdingFolder, "skins", $"{animation}.json"));
-        var infos = JsonConvert.DeserializeObject<SpriteInfo[]>(json);
-
-        // Import texture from skins folder
-        var bytes = File.ReadAllBytes(Path.Combine(FileHandler.ModdingFolder, "skins", $"{animation}.png"));
-        var texture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
-        texture.LoadImage(bytes, false);
-        texture.filterMode = FilterMode.Point;
-
-        // Load each sprite from the texture based on its info
-        foreach (var info in infos)
-        {
-            ModLog.Info($"Importing {info.Name}");
-
-            var rect = new Rect(info.Position, 0, info.Width, info.Height);
-            var sprite = Sprite.Create(texture, rect, new Vector2(info.Pivot, 0), info.PixelsPerUnit);
-            sprite.hideFlags = HideFlags.DontUnloadUnusedAsset;
-
-            _loadedSprites.Add(info.Name, sprite);
         }
     }
 }
---
+++ BlasII.CustomSkins/Importer.cs
@@ -0,0 +1,60 @@
+﻿using BlasII.ModdingAPI;
+using Newtonsoft.Json;
+using System.IO;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace BlasII.CustomSkins;
+
+/// <summary>
+/// Handles loading all custom sprites
+/// </summary>
+public class Importer(string skinsFolder)
+{
+    private readonly string _skinsFolder = skinsFolder;
+
+    public Dictionary<string, Sprite> LoadAllSpritesheets()
+    {
+        ModLog.Warn("Starting Import...");
+
+        // Create output dictionary
+        var output = new Dictionary<string, Sprite>();
+
+        // Create skins folder
+        //string dir = Path.Combine(Main.CustomSkins.FileHandler.ModdingFolder, "skins");
+        Directory.CreateDirectory(_skinsFolder);
+
+        // Get all json files in the skins folder
+        foreach (var file in Directory.GetFiles(_skinsFolder, "*.json", SearchOption.TopDirectoryOnly))
+        {
+            LoadSpritesheet(Path.GetFileNameWithoutExtension(file), output);
+        }
+
+        return output;
+    }
+
+    private void LoadSpritesheet(string animation, Dictionary<string, Sprite> output)
+    {
+        // Import info list from skins folder
+        var json = File.ReadAllText(Path.Combine(_skinsFolder, $"{animation}.json"));
+        var infos = JsonConvert.DeserializeObject<SpriteInfo[]>(json);
+
+        // Import texture from skins folder
+        var bytes = File.ReadAllBytes(Path.Combine(_skinsFolder, $"{animation}.png"));
+        var texture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+        texture.LoadImage(bytes, false);
+        texture.filterMode = FilterMode.Point;
+
+        // Load each sprite from the texture based on its info
+        foreach (var info in infos)
+        {
+            ModLog.Info($"Importing {info.Name}");
+
+            var rect = new Rect(info.Position, 0, info.Width, info.Height);
+            var sprite = Sprite.Create(texture, rect, new Vector2(info.Pivot, 0), info.PixelsPerUnit);
+            sprite.hideFlags = HideFlags.DontUnloadUnusedAsset;
+
+            output.Add(info.Name, sprite);
+        }
+    }
+}
