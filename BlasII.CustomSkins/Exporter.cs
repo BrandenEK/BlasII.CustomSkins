e18f74836ee460c26644f9a8d6ba03b6a73d77ff
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-23 08:52:13 -0500

    export: clean up old attempts

--- BlasII.CustomSkins/Exporter.cs
+++ BlasII.CustomSkins/Exporter.cs
@@ -1,6 +1,5 @@
 ﻿using BlasII.CustomSkins.Extensions;
 using BlasII.ModdingAPI;
-using Il2CppDelaunay;
 using MelonLoader;
 using Newtonsoft.Json;
 using System.Collections;
@@ -39,126 +38,36 @@ internal class Exporter(string path)
             Export(group.Key, sprites);
             yield return null;
         }
+
+        ModLog.Warn("Finished export");
     }
-
-    //private void Export(string animation, IEnumerable<Sprite> sprites)
-    //{
-    //    ModLog.Info($"Exporting {animation}");
-
-    //    // Create entire animation texture
-    //    int width = (int)sprites.Sum(x => x.rect.width);
-    //    int height = (int)sprites.Max(x => x.rect.height);
-    //    if (width > 16384 || height > 16384)
-    //    {
-    //        ModLog.Error("Size too big, failed to export texture");
-    //        return;
-    //    }
-    //    Texture2D tex = new Texture2D(width, height);
-    //    Object.Destroy(tex);
-
-    //    // Fill transparent
-    //    for (int i = 0; i < width; i++)
-    //        for (int j = 0; j < height; j++)
-    //            tex.SetPixel(i, j, new Color32(0, 0, 0, 0));
-
-    //    // Create empty info list
-    //    SpriteInfo[] infos = new SpriteInfo[sprites.Count()];
-
-    //    // Copy each sprite to the texture and save info
-    //    int x = 0, y = 0, idx = 0;
-    //    foreach (var sprite in sprites)
-    //    {
-    //        //ModLog.Info($"Exporting {sprite.name}");
-    //        int w = (int)sprite.rect.width;
-    //        int h = (int)sprite.rect.height;
-    //        Graphics.CopyTexture(sprite.GetSlicedTexture(), 0, 0, 0, 0, w, h, tex, 0, 0, x, 0);
-
-    //        infos[idx] = new SpriteInfo()
-    //        {
-    //            Name = sprite.name,
-    //            PixelsPerUnit = (int)sprite.pixelsPerUnit,
-    //            Position = new Vector(x, 0),
-    //            Size = new Vector(w, h),
-    //            Pivot = new Vector(sprite.pivot.x / w, sprite.pivot.y / h),
-    //        };
-
-    //        x += w;
-    //        idx++;
-    //    }
-
-    //    // Save texture to file
-    //    string texturePath = Path.Combine(_exportFolder, $"{animation}.png");
-    //    File.WriteAllBytes(texturePath, tex.EncodeToPNG());
-
-    //    // Save info list to file
-    //    string infoPath = Path.Combine(_exportFolder, $"{animation}.json");
-    //    File.WriteAllText(infoPath, JsonConvert.SerializeObject(infos, Formatting.Indented));
-    //}
 
     private void Export(string animation, IEnumerable<Sprite> sprites)
     {
         ModLog.Info($"Exporting {animation}");
 
-        // Create entire animation texture
-        Dictionary<Sprite, SpriteInfo> infos = GetInfos(sprites);
+        // Create sprite infos and texture
+        Dictionary<Sprite, SpriteInfo> infos = CreateSpriteInfos(sprites);
+        Texture2D texture = CreateTexture(infos.Values);
 
-        int width = (int)infos.Values.Max(info => info.Position.X + info.Size.X);
-        int height = (int)infos.Values.Max(info => info.Position.Y + info.Size.Y);
-        if (width > 16384 || height > 16384)
-        {
-            ModLog.Error("Size too big, failed to export texture");
-            return;
-        }
-        Texture2D tex = new Texture2D(width, height);
-        Object.Destroy(tex);
-
-        // Fill transparent
-        for (int i = 0; i < width; i++)
-            for (int j = 0; j < height; j++)
-                tex.SetPixel(i, j, new Color32(0, 0, 0, 0));
-
-        // Create empty info list
-        //SpriteInfo[] infos = new SpriteInfo[sprites.Count()];
-
+        // Copy each sprite onto the texture
         foreach (var kvp in infos)
         {
             Sprite sprite = kvp.Key;
             SpriteInfo info = kvp.Value;
-            Graphics.CopyTexture(sprite.GetSlicedTexture(), 0, 0, 0, 0, (int)info.Size.X, (int)info.Size.Y, tex, 0, 0, (int)info.Position.X, (int)info.Position.Y);
+            Graphics.CopyTexture(sprite.GetSlicedTexture(), 0, 0, 0, 0, (int)info.Size.X, (int)info.Size.Y, texture, 0, 0, (int)info.Position.X, (int)info.Position.Y);
         }
-
-        // Copy each sprite to the texture and save info
-        //int x = 0, y = 0, idx = 0;
-        //foreach (var sprite in sprites)
-        //{
-        //    //ModLog.Info($"Exporting {sprite.name}");
-        //    int w = (int)sprite.rect.width;
-        //    int h = (int)sprite.rect.height;
-        //    Graphics.CopyTexture(sprite.GetSlicedTexture(), 0, 0, 0, 0, w, h, tex, 0, 0, x, 0);
-
-        //    //infos[idx] = new SpriteInfo()
-        //    //{
-        //    //    Name = sprite.name,
-        //    //    PixelsPerUnit = (int)sprite.pixelsPerUnit,
-        //    //    Position = new Vector(x, 0),
-        //    //    Size = new Vector(w, h),
-        //    //    Pivot = new Vector(sprite.pivot.x / w, sprite.pivot.y / h),
-        //    //};
-
-        //    x += w;
-        //    idx++;
-        //}
 
         // Save texture to file
         string texturePath = Path.Combine(_exportFolder, $"{animation}.png");
-        File.WriteAllBytes(texturePath, tex.EncodeToPNG());
+        File.WriteAllBytes(texturePath, texture.EncodeToPNG());
 
         // Save info list to file
         string infoPath = Path.Combine(_exportFolder, $"{animation}.json");
         File.WriteAllText(infoPath, JsonConvert.SerializeObject(infos.Values, Formatting.Indented));
     }
 
-    private Dictionary<Sprite, SpriteInfo> GetInfos(IEnumerable<Sprite> sprites)
+    private Dictionary<Sprite, SpriteInfo> CreateSpriteInfos(IEnumerable<Sprite> sprites)
     {
         var infos = new Dictionary<Sprite, SpriteInfo>();
 
@@ -193,111 +102,20 @@ internal class Exporter(string path)
         return infos;
     }
 
-    private Vector GetMaximumSize(IEnumerable<Sprite> sprites)
+    private Texture2D CreateTexture(IEnumerable<SpriteInfo> infos)
     {
-        int totalWidth = 0, totalHeight = 0, rowHeight = 0, x = 0, y = 0;
-        foreach (var sprite in sprites)
-        {
-            int w = (int)sprite.rect.width;
-            int h = (int)sprite.rect.height;
-            x += w;
+        int width = (int)infos.Max(info => info.Position.X + info.Size.X);
+        int height = (int)infos.Max(info => info.Position.Y + info.Size.Y);
+        Texture2D tex = new Texture2D(width, height);
+        Object.Destroy(tex);
 
-            // Move to next row
-            if (x > MAX_SIZE)
-            {
-                x = w;
-                y += rowHeight;
-                totalHeight += rowHeight;
-                rowHeight = 0;
-            }
+        // Fill transparent
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                tex.SetPixel(i, j, new Color32(0, 0, 0, 0));
 
-            // Update max row height
-            if (h > rowHeight)
-                rowHeight = h;
-
-            // Update max spritesheet width
-            if (x > totalWidth)
-                totalWidth = x;
-        }
-
-        totalHeight += rowHeight;
-        return new Vector(totalWidth, totalHeight);
-    }
-
-    //private Vector GetMaximumSize(IEnumerable<Sprite> sprites)
-    //{
-    //    int totalWidth = 0, totalHeight = 0, rowHeight = 0, x = 0, y = 0;
-    //    foreach (var sprite in sprites)
-    //    {
-    //        int w = (int)sprite.rect.width;
-    //        int h = (int)sprite.rect.height;
-
-    //        // Pretend to copy sprite onto global texture
-
-    //        x += w;
-
-    //        // Validate next position
-
-    //        if (x > MAX_SIZE)
-    //        {
-    //            x = 0;
-    //            y += rowHeight;
-    //            totalHeight += rowHeight;
-    //            rowHeight = 0;
-    //        }
-
-    //        // Update max row height
-    //        if (h > rowHeight)
-    //            rowHeight = h;
-
-    //        x += w;
-
-    //        // Update max spritesheet width
-    //        if (x + w > totalWidth)
-    //            totalWidth = x + w;
-    //    }
-
-    //    totalHeight += rowHeight;
-    //    return new Vector(totalWidth, totalHeight);
-    //}
-
-    private IEnumerable<SpriteWithLocation> FindLocations(IEnumerable<Sprite> sprites)
-    {
-        var locations = new List<SpriteWithLocation>();
-
-        int x = 0, y = 0, maxRowHeight = 0;
-        foreach (var sprite in sprites)
-        {
-            int w = (int)sprite.rect.width;
-            int h = (int)sprite.rect.height;
-
-            if (h > maxRowHeight)
-                maxRowHeight = h;
-
-            if (x + w > MAX_SIZE)
-            {
-                x = 0;
-                y += maxRowHeight;
-                maxRowHeight = 0;
-            }
-
-            locations.Add(new SpriteWithLocation()
-            {
-                Sprite = sprite,
-                Location = new Vector(x, y)
-            });
-
-            x += w;
-        }
-
-        return locations;
+        return tex;
     }
 
     private const int MAX_SIZE = 2048;
-
-    class SpriteWithLocation()
-    {
-        public Sprite Sprite { get; init; }
-        public Vector Location { get; init; }
-    }
 }
