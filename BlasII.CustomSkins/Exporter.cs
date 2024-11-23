b9657d87f249fdebf6dcca26fae7b63e06eba33c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-23 07:43:59 -0500

    export: try layering texture

--- BlasII.CustomSkins/Exporter.cs
+++ BlasII.CustomSkins/Exporter.cs
@@ -26,7 +26,7 @@ internal class Exporter(string path)
     private IEnumerator ExportCoroutine(Dictionary<string, Sprite> export)
     {
         // Group sprites by name
-        var groups = export.GroupBy(x => x.Key[0..x.Key.LastIndexOf('_')]);
+        var groups = export.Take(100).GroupBy(x => x.Key[0..x.Key.LastIndexOf('_')]);
 
         // Export each individual spritesheet
         foreach (var group in groups)
@@ -42,7 +42,7 @@ internal class Exporter(string path)
 
     private void Export(string animation, IEnumerable<Sprite> sprites)
     {
-        ModLog.Info($"Exporting {animation} [{sprites.Count()}]");
+        ModLog.Info($"Exporting {animation}");
 
         // Create entire animation texture
         int width = (int)sprites.Sum(x => x.rect.width);
@@ -87,10 +87,120 @@ internal class Exporter(string path)
 
         // Save texture to file
         string texturePath = Path.Combine(_exportFolder, $"{animation}.png");
-        //File.WriteAllBytes(texturePath, tex.EncodeToPNG());
+        File.WriteAllBytes(texturePath, tex.EncodeToPNG());
 
         // Save info list to file
         string infoPath = Path.Combine(_exportFolder, $"{animation}.json");
-        //File.WriteAllText(infoPath, JsonConvert.SerializeObject(infos, Formatting.Indented));
+        File.WriteAllText(infoPath, JsonConvert.SerializeObject(infos, Formatting.Indented));
+    }
+
+
+
+    private Vector GetMaximumSize(IEnumerable<Sprite> sprites)
+    {
+        int totalWidth = 0, totalHeight = 0, rowHeight = 0, x = 0, y = 0;
+        foreach (var sprite in sprites)
+        {
+            int w = (int)sprite.rect.width;
+            int h = (int)sprite.rect.height;
+            x += w;
+
+            // Move to next row
+            if (x > MAX_SIZE)
+            {
+                x = w;
+                y += rowHeight;
+                totalHeight += rowHeight;
+                rowHeight = 0;
+            }
+
+            // Update max row height
+            if (h > rowHeight)
+                rowHeight = h;
+
+            // Update max spritesheet width
+            if (x > totalWidth)
+                totalWidth = x;
+        }
+
+        totalHeight += rowHeight;
+        return new Vector(totalWidth, totalHeight);
+    }
+
+    //private Vector GetMaximumSize(IEnumerable<Sprite> sprites)
+    //{
+    //    int totalWidth = 0, totalHeight = 0, rowHeight = 0, x = 0, y = 0;
+    //    foreach (var sprite in sprites)
+    //    {
+    //        int w = (int)sprite.rect.width;
+    //        int h = (int)sprite.rect.height;
+
+    //        // Pretend to copy sprite onto global texture
+
+    //        x += w;
+
+    //        // Validate next position
+
+    //        if (x > MAX_SIZE)
+    //        {
+    //            x = 0;
+    //            y += rowHeight;
+    //            totalHeight += rowHeight;
+    //            rowHeight = 0;
+    //        }
+
+    //        // Update max row height
+    //        if (h > rowHeight)
+    //            rowHeight = h;
+
+    //        x += w;
+
+    //        // Update max spritesheet width
+    //        if (x + w > totalWidth)
+    //            totalWidth = x + w;
+    //    }
+
+    //    totalHeight += rowHeight;
+    //    return new Vector(totalWidth, totalHeight);
+    //}
+
+    private IEnumerable<SpriteWithLocation> FindLocations(IEnumerable<Sprite> sprites)
+    {
+        var locations = new List<SpriteWithLocation>();
+
+        int x = 0, y = 0, maxRowHeight = 0;
+        foreach (var sprite in sprites)
+        {
+            int w = (int)sprite.rect.width;
+            int h = (int)sprite.rect.height;
+
+            if (h > maxRowHeight)
+                maxRowHeight = h;
+
+            if (x + w > MAX_SIZE)
+            {
+                x = 0;
+                y += maxRowHeight;
+                maxRowHeight = 0;
+            }
+
+            locations.Add(new SpriteWithLocation()
+            {
+                Sprite = sprite,
+                Location = new Vector(x, y)
+            });
+
+            x += w;
+        }
+
+        return locations;
+    }
+
+    private const int MAX_SIZE = 1024;
+
+    class SpriteWithLocation()
+    {
+        public Sprite Sprite { get; init; }
+        public Vector Location { get; init; }
     }
 }
