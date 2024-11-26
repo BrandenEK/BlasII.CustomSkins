6f233bf2b3f6cf76154430eac60c53b4a940ec9b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-25 20:34:45 -0500

    find: add IFinder

---
+++ BlasII.CustomSkins/Finders/IFinder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlasII.CustomSkins.Finders;
+
+/// <summary>
+/// Handles finding all player sprites from the game
+/// </summary>
+public interface IFinder
+{
+    /// <summary>
+    /// Finds all player sprites
+    /// </summary>
+    public Dictionary<string, Sprite> FindAll();
+}
