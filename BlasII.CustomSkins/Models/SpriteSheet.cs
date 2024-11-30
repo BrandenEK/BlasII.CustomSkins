e446fb5ea98ba05953d1816d8b7571abe2f376c1
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-30 10:48:02 -0500

    export: try a new method
    Atempts to combine each frame/animation into the next texture immedietaly, instead of storing them until the end

--- BlasII.CustomSkins/Models/SpriteSheet.cs
+++ BlasII.CustomSkins/Models/SpriteSheet.cs
@@ -23,3 +23,29 @@ public class SpriteSheet
     /// </summary>
     public IEnumerable<SpriteInfo> Infos { get; set; }
 }
+
+/// <summary>
+/// A texture, along with info about each sprite
+/// </summary>
+public class SpriteSheetWithPosition
+{
+    /// <summary>
+    /// The name the spritesheet should be saved as
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The texture
+    /// </summary>
+    public Texture2D Texture { get; set; }
+
+    /// <summary>
+    /// List of SpriteInfos about every sprite
+    /// </summary>
+    public IEnumerable<SpriteInfo> Infos { get; set; }
+
+    /// <summary>
+    /// The pixel position where the next spritesheet should go
+    /// </summary>
+    public Vector NextPosition { get; set; }
+}
