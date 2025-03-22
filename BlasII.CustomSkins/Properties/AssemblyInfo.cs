87e090683ceee273d0a02d33f798b4dd8982f9df
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-03-22 07:15:47 -0400

    core: upgrade dependencies

--- BlasII.CustomSkins/Properties/AssemblyInfo.cs
+++ BlasII.CustomSkins/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.CustomSkins;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.CustomSkins.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "1.1.0", "Cheat Console", "1.2.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.0.0", "Cheat Console", "1.3.0")]
