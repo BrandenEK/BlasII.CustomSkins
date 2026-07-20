0a0d0d1ac86d103139960f7a6debc3ebb476dd3e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-07-19 22:09:24 -0400

    core: read info file

--- BlasII.CustomSkins/SkinImporter.cs
+++ BlasII.CustomSkins/SkinImporter.cs
@@ -1,5 +1,8 @@
-﻿using BlasII.ModdingAPI;
+﻿using BlasII.CustomSkins.Models;
+using BlasII.ModdingAPI;
+using Newtonsoft.Json;
 using System.IO;
+using UnityEngine;
 
 namespace BlasII.CustomSkins;
 
@@ -13,7 +16,42 @@ internal class SkinImporter
         ModLog.Warn("Found folders");
         foreach (string path in Directory.GetDirectories(directory))
         {
-            ModLog.Info(path);
+            try
+            {
+                LoadSkin(path);
+            }
+            catch (System.Exception ex)
+            {
+                ModLog.Error($"Failed to load skin from {path} ({ex.Message})");
+            }
         }
+    }
+
+    private void LoadSkin(string directory)
+    {
+        string infoPath = Path.Combine(directory, "info.json");
+        string texturePath = Path.Combine(directory, "texture.png");
+
+        if (!File.Exists(infoPath))
+            throw new System.Exception("No info file was present");
+
+        if (!File.Exists(texturePath))
+            throw new System.Exception("No texture file was present");
+
+        var info = LoadInfo(infoPath);
+        var texture = LoadTexture(texturePath);
+
+        ModLog.Warn($"{info.Id} {info.Name} by {info.Author}");
+    }
+
+    private SkinInfo LoadInfo(string path)
+    {
+        string text = File.ReadAllText(path);
+        return JsonConvert.DeserializeObject<SkinInfo>(text);
+    }
+
+    private Texture2D LoadTexture(string path)
+    {
+        return null;
     }
 }
