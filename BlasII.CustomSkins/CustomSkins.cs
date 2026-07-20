9a4fe36952867fd4492ddc5a70285cf929b25200
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-07-19 21:46:52 -0400

    core: create and setup new project (#47)
    * core: add new project
    
    * core: setup new project

---
+++ BlasII.CustomSkins/CustomSkins.cs
@@ -0,0 +1,16 @@
+﻿using BlasII.ModdingAPI;
+
+namespace BlasII.CustomSkins;
+
+/// <summary>
+/// Allows using custom player skins made by the community
+/// </summary>
+public class CustomSkins : BlasIIMod
+{
+    internal CustomSkins() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        // Perform initialization here
+    }
+}
