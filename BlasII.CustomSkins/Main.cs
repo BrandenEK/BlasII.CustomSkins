00be0b279d7019a1122b941bae69ed7b57c72cc2
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-07-19 21:41:44 -0400

    core: add new project

---
+++ BlasII.CustomSkins/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.CustomSkins;
+
+internal class Main : MelonMod
+{
+    public static CustomSkins CustomSkins { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        CustomSkins = new CustomSkins();
+    }
+}
