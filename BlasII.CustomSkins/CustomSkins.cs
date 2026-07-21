18081edf15e592acfdfbf56048264b8c7c5e989e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-07-20 22:28:35 -0400

    core: store id in save data

--- BlasII.CustomSkins/CustomSkins.cs
+++ BlasII.CustomSkins/CustomSkins.cs
@@ -1,5 +1,6 @@
 ﻿using BlasII.CustomSkins.Models;
 using BlasII.ModdingAPI;
+using BlasII.ModdingAPI.Persistence;
 using System.Collections.Generic;
 using System.IO;
 
@@ -8,11 +9,12 @@ namespace BlasII.CustomSkins;
 /// <summary>
 /// Allows using custom player skins made by the community
 /// </summary>
-public class CustomSkins : BlasIIMod
+public class CustomSkins : BlasIIMod, IGlobalPersistentMod<SkinGlobalSaveData>
 {
     internal CustomSkins() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
     private List<SkinData> _loadedSkins;
+    private string _selectedSkin = string.Empty;
 
     /// <summary>
     /// Loads all skins from the modding folder
@@ -21,6 +23,26 @@ public class CustomSkins : BlasIIMod
     {
         var importer = new SkinImporter();
         _loadedSkins = importer.LoadAllSkins(Path.Combine(FileHandler.ModdingFolder, "custom_skins"));
+    }
+
+    /// <summary>
+    /// Saves the current selected custom skin
+    /// </summary>
+    public SkinGlobalSaveData SaveGlobal()
+    {
+        return new SkinGlobalSaveData()
+        {
+            SelectedSkin = _selectedSkin
+        };
+    }
+
+    /// <summary>
+    /// Loads the current selected custom skin
+    /// </summary>
+    public void LoadGlobal(SkinGlobalSaveData data)
+    {
+        ModLog.Error("Loading the skin: " + data.SelectedSkin);
+        _selectedSkin = data.SelectedSkin ?? string.Empty;
     }
 
     internal IEnumerable<SkinData> GetAllSkins() => _loadedSkins;
---
+++ BlasII.CustomSkins/SkinGlobalSaveData.cs
@@ -0,0 +1,14 @@
+﻿using BlasII.ModdingAPI.Persistence;
+
+namespace BlasII.CustomSkins;
+
+/// <summary>
+/// Stores global data for the Custom Skins mod
+/// </summary>
+public class SkinGlobalSaveData : GlobalSaveData
+{
+    /// <summary>
+    /// The id of the currently selected skin
+    /// </summary>
+    public string SelectedSkin { get; set; } = string.Empty;
+}
